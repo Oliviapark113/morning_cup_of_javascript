94c8818eb0fd86b685d7bceb5068e947e90b45a7
Nicholas Vrtis <nvav91@gmail.com>
2021-04-14 04:01:55 +0200

    news

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,12 +3,22 @@ import Container from "../components/container/container"
 import Row from "../components/row/row"
 import Col from "../components/col/col"
 
-const Home =() =>(
+const Home =() =>{
+    var url = `https://newsapi.org/v2/everything?' +
+    'q=Apple&' +
+    'from=2021-04-14&' +
+    'sortBy=popularity&' +
+    'apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
+    
+   
+   
+   
+    return (
     <Container>
         <Row>
         <Col>Home</Col>
         </Row>
     </Container>
 )
-
+}
 export default Home
