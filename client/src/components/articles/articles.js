92a7af2498cf08ac03a09796c553952e9020c942
Nicholas Vrtis <nvav91@gmail.com>
2021-04-15 01:00:26 +0200

    posting news

---
+++ client/src/components/articles/articles.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./style.css"
+
+
+// deconstruct props and makes the cards for projects
+const Article = (
+  {title, img, author, desc, content, src, link}
+  ) => {
+console.log(content)
+const image = <img src={img} className="img-fluid img" alt={title}/>
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        {img==="" ? null :image }   
+        {author==="" ? null :<p className="card-text">Author: {author}</p>} 
+        {desc==="" ? null :<p className="card-text">Description: {desc}</p>}    
+        {content==="" ? null :<p className="card-text">Description: {content}</p>}      
+        
+        <a href={link} target="_blank" rel="noreferrer" className="btn btn-primary">Link to Article at {src}</a>
+      </div>
+    </div>
+  )
+}
+
+export default Article
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,22 +1,55 @@
-import React from "react"
+import React, { useState,useEffect } from 'react'
 import Container from "../components/container/container"
 import Row from "../components/row/row"
 import Col from "../components/col/col"
+import axios from "axios"
+import { useAuth0 } from "@auth0/auth0-react";
+import Article from "../components/articles/articles"
 
 const Home =() =>{
-    var url = `https://newsapi.org/v2/everything?' +
-    'q=Apple&' +
-    'from=2021-04-14&' +
-    'sortBy=popularity&' +
-    'apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
+    const [articles, setArticles] = useState([])
+    const { isAuthenticated } = useAuth0();
+
+    useEffect(() => {
+        getNews()
+      }, []);
     
-   
+    function getNews() {
+        const url = `https://newsapi.org/v2/top-headlines?category=technology&country=us&country=gb&${isAuthenticated?"pageSize=100&":""}apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
+        axios.get(url)
+          .then(resp => {
+              console.log(resp.data.articles)
+            setArticles(resp.data.articles)
+          }).catch(err => console.log(err))
+      }
+    
+
+      // title= title
+      //   img= urlToImage if null
+      // author if
+      // desc if
+      // content if (read more)
+      // src
+      // link = url
    
    
     return (
     <Container>
         <Row>
-        <Col>Home</Col>
+        {articles.map((article, index) => (
+          <Col className="col" key={index}>
+            <Article
+              title={article.title}
+              img={article.urlToImage === null? "": `${article.urlToImage}`}
+              author={article.author === null? "": `${article.author}`}
+              desc={article.description === null? "": `${article.description}`}
+              content={article.content===null? "":`${article.content}`}
+              src={article.source.name}
+              link={article.link}
+            />
+          </Col>
+        )
+        )}
         </Row>
     </Container>
 )
