b50620a80a153273aea3815aa9ff98f95a7d970c
Nicholas Vrtis <nvav91@gmail.com>
2021-04-21 17:23:56 +0200

    added body and date to comments

--- client/src/components/inspiration-card/inspiration-card.js
+++ client/src/components/inspiration-card/inspiration-card.js
@@ -1,6 +1,6 @@
 import React from "react"
 // import { useAuth0 } from "@auth0/auth0-react";
-// import Col from "../col/col"
+import Col from "../col/col"
 // import Row from "../row/row"
 
 const CommentsCard = (props) => {
@@ -8,15 +8,25 @@ const CommentsCard = (props) => {
     console.log(props)
     return (
 
-
-
+<>
+<Col className="col-2">
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{props.userName}</h5>
 
                 </div>
             </div>
+</Col>
+<Col className="col-10">
+<div className="card">
+                <div className="card-body">
+                    <p>{props.date}</p>
+                    <input type="text" value={props.body} disabled></input>
 
+                </div>
+            </div>
+</Col>
+</>
 
     )
 }
