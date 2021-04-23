74f937a2e9d4e8339d2a316879a3710c581a76e1
Nicholas Vrtis <nvav91@gmail.com>
2021-04-23 19:15:25 +0200

    adding checkbox styling

--- client/src/components/inspiration-checkboxes/inspiration-checkboxes.js
+++ client/src/components/inspiration-checkboxes/inspiration-checkboxes.js
@@ -7,14 +7,14 @@ const InspirationCheckboxes = (props) => {
 
   return (
     <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-    <input type="checkbox" defaultChecked="checked" className="btn-check" name="npm" id="btncheck1" {...props} />
-    <label className="btn btn-outline-primary" htmlFor="btncheck1">npm</label>
+    <input type="checkbox" defaultChecked="checked" className="btn-check inscheck" name="npm" id="btncheck1" {...props} />
+    <label className=" inscheck" htmlFor="btncheck1">npm</label>
   
-    <input type="checkbox" defaultChecked="checked" className="btn-check" name="api" id="btncheck2" {...props}/>
-    <label className="btn btn-outline-primary" htmlFor="btncheck2">api</label>
+    <input type="checkbox" defaultChecked="checked" className="btn-check inscheck" name="api" id="btncheck2" {...props}/>
+    <label className=" inscheck" htmlFor="btncheck2">api</label>
   
-    <input type="checkbox" defaultChecked="checked" className="btn-check" name="framework" id="btncheck3" {...props}/>
-    <label className="btn btn-outline-primary" htmlFor="btncheck3">framework</label>
+    <input type="checkbox" defaultChecked="checked" className="btn-check inscheck" name="framework" id="btncheck3" {...props}/>
+    <label className="  inscheck" htmlFor="btncheck3">framework</label>
 
   </div>
   )
