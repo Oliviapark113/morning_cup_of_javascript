418fdd4eef52e77d2fcf418dae61bbdc56e23787
Nicholas Vrtis <nvav91@gmail.com>
2021-04-23 22:03:05 +0200

    added styling for insp comments

--- client/src/components/inspiration-card/inspiration-card.js
+++ client/src/components/inspiration-card/inspiration-card.js
@@ -12,19 +12,19 @@ const CommentsCard = (props) => {
     return (
 <>
 <Col className="col-2">
-            <div className="card">
+            <div className="card inspComment">
                 <div className="card-body">
                     <h5 className="card-title">{props.userName}</h5>
-                    {user.sub===props.id?<button onClick={props.onClick}>{'\u270E'}</button>:null}
-                    {user.sub===props.id?<button id={props._id} onClick={props.onClick2} >{'\u2715'}</button>:null}
+                    {user.sub===props.id?<button className='commentBtn' onClick={props.onClick}>{'\u270E'}</button>:null}
+                    {user.sub===props.id?<button className='commentBtn' id={props._id} onClick={props.onClick2} >{'\u2715'}</button>:null}
                 </div>
             </div>
 </Col>
 <Col className="col-10">
 <div className="card">
-                <form className="card-body" onSubmit={props.onSubmit}>
+                <form className="card-body inspComment" onSubmit={props.onSubmit}>
                     <p>{props.date}</p>
-                    <input type="text" id={props._id}placeholder={props.body}  disabled></input>
+                    <textarea className='form-control' type="text" id={props._id}placeholder={props.body}  disabled/>
                     <button type="submit" className="editButton">OK</button>
 
                 </form>
