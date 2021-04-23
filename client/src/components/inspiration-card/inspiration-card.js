1a19e5e5dfb490be73b9713e8233fa78fed0fc4f
Nicholas Vrtis <nvav91@gmail.com>
2021-04-23 23:31:31 +0200

    dynamic comments

--- client/src/components/inspiration-card/inspiration-card.js
+++ client/src/components/inspiration-card/inspiration-card.js
@@ -1,10 +1,31 @@
 import React from "react"
 import { useAuth0 } from "@auth0/auth0-react";
 import Col from "../col/col"
+import styled from 'styled-components'
 
 
 const CommentsCard = (props) => {
+    let StyleTextArea =  styled.textarea`
+    height: 100fpx;
+    `
 
+    // console.log(props.body.length)
+    if(props.body.length>=500) {
+        StyleTextArea = styled.textarea`
+        height: 200px;
+        `
+    }
+    else if(props.body.length>=400) {
+        StyleTextArea = styled.textarea`
+        height: 150px;
+        `
+    }
+    else if(props.body.length>=300) {
+        StyleTextArea = styled.textarea`
+        height: 125px;
+        `
+    }
+    
     const { user } = useAuth0();
 
     // console.log(props)
@@ -24,8 +45,8 @@ const CommentsCard = (props) => {
 <div className="card">
                 <form className="card-body inspComment" onSubmit={props.onSubmit}>
                     <p>{props.date}</p>
-                    <textarea className='form-control' type="text" id={props._id}placeholder={props.body}  disabled/>
-                    <button type="submit" className="editButton">OK</button>
+                    <StyleTextArea className='form-control' type="text" id={props._id}placeholder={props.body}  disabled/>
+                    <button type="submit" className="editButton editButton2">OK</button>
 
                 </form>
             </div>
