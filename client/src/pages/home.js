e7eff3629c0f0668258e53c3228b4319cad462d6
Nicholas Vrtis <nvav91@gmail.com>
2021-04-25 16:12:10 +0200

    broken api call

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -18,43 +18,66 @@ const Home = () => {
 
     useEffect(() => {
         getNews()
-    },[isAuthenticated]);
+    }, [isAuthenticated]);
 
     function getNews() {
-        const url = `https://gnews.io/api/v4/top-headlines?topic=technology&lang=en&token=${process.env.REACT_APP_NEWS_API_KEY}`;
+        const url = `https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=${process.env.REACT_APP_NEWS_API_KEY2}`;
+
         axios.get(url)
             .then(resp => {
-                console.log(resp)
-                setArticles(resp.data.articles)
+                console.log(resp.data.results)
+                resp.data.results.forEach(element => {
+                    console.log(element)
+                     setArticles([...articles,{
+                        title: element.title,
+                        image: element.multimedia[2].url,
+                        publishedAt: element.created_date,
+                        description: element.abstract,
+                        link: element.url
+                    }])
+                });
+                // setArticles(resp.data.articles)
             }).catch(err => console.log(err))
-    }
 
-    function onClickHandler (e) {
-e.preventDefault()
-if(e.target.innerHTML=== "Read More") {
-    e.target.innerHTML = "Read Less"
-}
-else {
-    e.target.innerHTML="Read More"
-}
+        // if (isAuthenticated) {
+        //     const url2 = `https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=${process.env.REACT_APP_NEWS_API_KEY2}`
+        //     axios.get(url2)
+        //         .then(resp => {
+        //             console.log(resp.data.results)
 
-if(e.target.parentNode.children[4].innerHTML==="Read Less")
-e.target.parentNode.children[3].classList.toggle("articleReadMore")
-else {
-    e.target.parentNode.children[4].classList.toggle("articleReadMore")
-}
-
-
+        //         }).catch(err => console.log(err))
+        // }
 
 
     }
 
+    function onClickHandler(e) {
+        e.preventDefault()
+        if (e.target.innerHTML === "Read More") {
+            e.target.innerHTML = "Read Less"
+        }
+        else {
+            e.target.innerHTML = "Read More"
+        }
+
+        if (e.target.parentNode.children[4].innerHTML === "Read Less")
+            e.target.parentNode.children[3].classList.toggle("articleReadMore")
+        else {
+            e.target.parentNode.children[4].classList.toggle("articleReadMore")
+        }
+
+
+
+
+    }
+
+    console.log(typeof articles)
     return (
         <Container>
             <Row>
                 <Col className="col-12">
                     <p className="dateAndtime">
-            {newDate}
+                        {newDate}
 
                     </p>
 
@@ -62,12 +85,12 @@ else {
             </Row>
             <Row>
                 {articles.map((article, index) => {
-                    if(article.image.includes(".jpg")||article.image.includes(".png")){
+                    if (article.image.includes(".jpg") || article.image.includes(".png")) {
                     } else {
-                         article.image = ""
+                        article.image = ""
                     }
-        console.log()
-                const date = article.publishedAt.split("T")[0]
+                    console.log()
+                    const date = article.publishedAt.split("T")[0]
                     return (
                         <Col className="col-3" key={index}>
                             <Article
@@ -76,14 +99,14 @@ else {
                                 date={article.publishedAt === null ? "" : `${date}`}
                                 desc={article.description === null ? "" : `${article.description}`}
                                 content={article.content === null ? "" : `${article.content}`}
-                                src={article.source.name}
+                                // src={article.source.name === "New York Times" ? "" : `${article.source.name}`}
                                 link={article.url}
                                 onClick={onClickHandler}
-                                
-                                />
+
+                            />
                         </Col>
                     )
-                    
+
                 }
                 )}
             </Row>
