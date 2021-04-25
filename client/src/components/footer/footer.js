8fec856c7ff347fe0f90d5d569040cd3d4f08924
Olivia Park <oliviaypark113@gmail.com>
2021-04-25 17:11:43 -0400

    footer styling updated

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -8,24 +8,28 @@ function PageFooter() {
     return (<footer>
         <Row className="footer-row">
             <Col className="col-md-10">
-                <h3>Developers:</h3>
+                <p className="developers">Developers:</p>
                 <ul className="footer-list">
-                    <li className="footer-item">
-                    <a href='https://github.com/Oliviapark113' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a> Olivia Park 
+                <li className="footer-item">
+                    <a href='https://github.com/Pete-Scale' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a>Pete Scale 
                     </li>
                     <li className="footer-item">
-                    <a href='https://github.com/Pete-Scale' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a> Pete Scale 
+                    <a href='https://github.com/Oliviapark113' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a>Olivia Park
                     </li>
                     <li className="footer-item">
-                    <a href='https://github.com/Nvrtis/Nvrtis.github.io' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a> Nicholis Vrtis 
+                    <a href='https://github.com/Nvrtis/Nvrtis.github.io' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a>Nicholis Vrtis 
                     </li>
                     <li className="footer-item">
-                    <a href='https://github.com/Geoff7709' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a> Geoffrey Zimmerman 
+                    <a href='https://github.com/Geoff7709' target="_blank" rel="noreferrer"><FaGithubSquare className="github-icon"/></a>Geoffrey Zimmerman 
                     </li>
                 </ul>
             </Col>
-            <Col className="col-md-2">
-            <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noreferrer"><img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="MIT Badge"/></a>
+            <Col className="col-md-2 license">
+            <button className="btn link-btn"><a href="https://opensource.org/licenses/MIT" target="_blank" rel="noreferrer"> <img src="https://img.shields.io/badge/License-MIT-blue?style=for-the-badge&logo=appveyor" alt="MIT Badge"/></a></button>
+            <br/>
+                <div classN="footer-copyright"> © 2021 Copyright:
+                </div>
+
             </Col>
         </Row>
     </footer>)
